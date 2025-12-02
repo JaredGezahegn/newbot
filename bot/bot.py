0e23ec8ba0e8a65992380a0c36054d8aad136222
JaredGezahegn <jaredgezahegn@gmail.com>
2025-12-02 11:43:37 +0300

    Add message handlers for /start, /help, and echo

--- bot/bot.py
+++ bot/bot.py
@@ -12,3 +12,41 @@ from telebot.types import (
 )
 
 bot = TeleBot(settings.BOT_TOKEN, parse_mode="HTML", threaded=False)
+
+
+# Message Handlers
+@bot.message_handler(commands=['start'])
+def start_command(message: Message):
+    """Handle /start command"""
+    user_name = message.from_user.first_name
+    welcome_text = f"""
+👋 <b>Hello {user_name}!</b>
+
+Welcome to the bot! I'm up and running on Vercel.
+
+Available commands:
+/start - Show this message
+/help - Get help
+    """
+    bot.reply_to(message, welcome_text)
+
+
+@bot.message_handler(commands=['help'])
+def help_command(message: Message):
+    """Handle /help command"""
+    help_text = """
+<b>📚 Help Menu</b>
+
+Available commands:
+• /start - Start the bot
+• /help - Show this help message
+
+Send me any message and I'll echo it back!
+    """
+    bot.reply_to(message, help_text)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message: Message):
+    """Echo all other messages"""
+    bot.reply_to(message, f"You said: {message.text}")
