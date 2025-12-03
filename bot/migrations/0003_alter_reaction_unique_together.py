7f1cb543fc44ac4e7c3f56b043700a4f82741aeb
JaredGezahegn <jaredgezahegn@gmail.com>
2025-12-03 11:55:31 +0300

    Add migration to fix reaction constraint

---
+++ bot/migrations/0003_alter_reaction_unique_together.py
@@ -0,0 +1,21 @@
+# Generated migration to change Reaction unique_together constraint
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bot', '0002_comment_confession_reaction_alter_user_options_and_more'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='reaction',
+            unique_together={('comment', 'user', 'reaction_type')},
+        ),
+        migrations.AlterIndexTogether(
+            name='reaction',
+            index_together={('comment', 'user', 'reaction_type')},
+        ),
+    ]
